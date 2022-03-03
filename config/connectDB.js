444579ded6b03d2e459afd5119ce4ed36d22b5bd
wafa rtibi <wafertibi@gmail.com>
2022-03-03 14:09:42 +0100

    changed

--- config/connectDB.js
+++ config/connectDB.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose")
 //connect db with server
 const connectDB=async()=>{
    try {
-    await mongoose.connect("mongodb://localhost:27017/MERN")  
+    await mongoose.connect(process.env.URI)  
     console.log("database connected successfully")
    } catch (error) {
     console.log("fail to connect")
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+// importation dotenv
+require("dotenv").config();
+
 // importation express
 const express = require("express");
 
 // creation d'instance
 const app = express();
-
-// importation dotenv
-require("dotenv").config();
 
 // importation data base
 const connectDB = require("./config/connectDB");
@@ -17,9 +17,8 @@ app.use(express.json());
 app.use("api/personne", require("./Route/personne")); //path initial du personne
 
 //creation of server
-// const pore = process.env.PORT;
-// const pore=process.env.PORT
-const pore=5000;
-app.listen(pore, (error) => {
-  error ? console.log(error) : console.log(`the server is running on:${pore}`);
+const port = process.env.PORT || 5000;
+
+app.listen(port, (error) => {
+  error ? console.log(error) : console.log(`the server is running on:${port}`);
 });
